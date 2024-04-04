1570acad037b572a9c5197e09cb1c19b5bf3864e
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-04 17:03:57 +0530

    Files added and setup done

---
+++ Meal Tracker/Food.swift
@@ -0,0 +1,8 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by student on 04/04/24.
+//
+
+import Foundation
---
+++ Meal Tracker/Meal.swift
@@ -0,0 +1,8 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by student on 04/04/24.
+//
+
+import Foundation
