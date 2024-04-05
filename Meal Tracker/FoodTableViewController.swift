1264e52eaf7ab2af7d2cd9e8d0b21de88c194365
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-05 10:27:32 +0530

    App Completed

--- Meal Tracker/FoodTableViewController.swift
+++ Meal Tracker/FoodTableViewController.swift
@@ -38,21 +38,31 @@ class FoodTableViewController: UITableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return meals.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals[section].food.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "foodCell", for: indexPath)
         
         // Configure the cell...
         
+        let meal = meals[indexPath.section]
+        let food = meal.food[indexPath.row]
+        
+        cell.textLabel?.text = food.name
+        cell.detailTextLabel?.text = food.description
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView , titleForHeaderInSection section : Int) -> String{
+        return meals[section].name
     }
     
 }
